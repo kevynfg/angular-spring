cdb30842e22fd41f25bf88bbbf53464690f5a786
Kevyn Gonçalves <kevynredlotus@gmail.com>
2022-05-25 20:58:36 -0300

    initial commit

---
+++ src/app/courses/courses.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesComponent } from './courses/courses.component';
+import { AppMaterialModule } from "../shared/app-material/app-material.module";
+import { CoursesService } from "./services/courses.service";
+
+@NgModule({
+  declarations: [
+    CoursesComponent
+  ],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule,
+    AppMaterialModule
+  ],
+  providers: [
+    CoursesService
+  ]
+})
+export class CoursesModule { }
---
+++ src/app/courses/services/courses.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Course } from '../model/course';
+import { HttpClient } from '@angular/common/http';
+import { take, tap } from "rxjs";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoursesService {
+  private readonly API = '/assets/courses.json';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getCourses() {
+    return this.httpClient
+      .get<Course[]>(this.API)
+      .pipe(
+        take(1),
+        tap((courses) => console.log('Courses', courses)),
+      )
+  }
+}
